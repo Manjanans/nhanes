9fc2e598346cb3973711243369c4926af2928c05
eypino <107155607+eypino@users.noreply.github.com>
2024-10-14 21:17:41 -0300

    tpot y metricas preliminares

---
+++ notebooks/best_pipeline_Colesterol_Total_suero_regression.py
@@ -0,0 +1,19 @@
+import numpy as np
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.tree import DecisionTreeRegressor
+
+# NOTE: Make sure that the outcome column is labeled 'target' in the data file
+tpot_data = pd.read_csv('PATH/TO/DATA/FILE', sep='COLUMN_SEPARATOR', dtype=np.float64)
+features = tpot_data.drop('target', axis=1)
+training_features, testing_features, training_target, testing_target = \
+            train_test_split(features, tpot_data['target'], random_state=42)
+
+# Average CV score on the training set was: -1440.3097385724218
+exported_pipeline = DecisionTreeRegressor(max_depth=6, min_samples_leaf=1, min_samples_split=15)
+# Fix random state in exported estimator
+if hasattr(exported_pipeline, 'random_state'):
+    setattr(exported_pipeline, 'random_state', 42)
+
+exported_pipeline.fit(training_features, training_target)
+results = exported_pipeline.predict(testing_features)
